1409ca85a58a4c92c875269c164b12c9eb7c2957
JoDMsoluth <jodmsoluth@gmail.com>
2022-12-28 19:40:20 +0900

    Feat: add clear color

--- cmake_project_example/src/main.cpp
+++ cmake_project_example/src/main.cpp
@@ -1,5 +1,27 @@
 #include <spdlog/spdlog.h>
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
+
+void OnFramebufferSizeChange(GLFWwindow* window, int width, int height) {
+  SPDLOG_INFO("framebuffer size change: ({} * {})", width, height);
+  glViewport(0, 0, width, height);
+}
+
+void OnKeyEvent(GLFWwindow* window,
+    int key, int scancode, int action, int mods) {
+    SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
+        key, scancode,
+        action == GLFW_PRESS ? "Pressed" :
+        action == GLFW_RELEASE ? "Released" :
+        action == GLFW_REPEAT ? "Repeat" : "Unknown",
+        mods & GLFW_MOD_CONTROL ? "C" : "-",
+        mods & GLFW_MOD_SHIFT ? "S" : "-",
+        mods & GLFW_MOD_ALT ? "A" : "-");
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
 
 int main(int argc, const char** argv) {
     // 시작을 알리는 로그
@@ -23,12 +45,40 @@ int main(int argc, const char** argv) {
         glfwTerminate();
         return -1;
     }
+    // 이 윈도우 Context를 사용하겠다는 의미
+    glfwMakeContextCurrent(window);
+
+    // glad를 활용한 opengl 함수 로딩
+    if(!gladLoaderLoadGL()) {
+        SPDLOG_ERROR("failed to initialize glad");
+        glfwTerminate();
+        return -1;
+    }
+    auto glVersion = glGetString(GL_VERSION);
+    SPDLOG_INFO("OpenGL context version: {}", glVersion);
+
+    // 이벤트 등록
+    // framebuffersizechange 이벤트는 처음 윈도우 생성시에는 발생하지 않는 이슈 수정
+    OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
+    glfwSetKeyCallback(window, OnKeyEvent);
 
     // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
     while (!glfwWindowShouldClose(window)) {
+        // 1/60초 마다 이벤트 수집
         glfwPollEvents();
-    }
+
+        // 윈도우 색상 렌더링 
+        glClearColor(0.0f, 0.1f, 0.2f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // 더블 버퍼링
+        // 그림이 그려지는 과정이 보여지지 않기 위해 화면의 렌더링하기 위하기 위한 버퍼는 2개 생성
+        // 백버퍼(화면에 보이지 않는 버퍼), 프론트버퍼(화면에 보이는 버퍼)
+        // 백버퍼에서 미리 그리고 프론트버퍼와 Swapping해서 화면에 보임
+        glfwSwapBuffers(window); 
+    }  
 
     glfwTerminate();
     return 0;
